506a2d3d3303f6d41e062192a52f69dae1ea5947
Vikramad98 <gaikwadvikramaditya@gmail.com>
2020-09-21 18:26:20 +0530

    commit 2

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string Scene;
+
+    public void SceneChange()
+    {
+        SceneManager.LoadScene(Scene);
+    }
+}
---
+++ Assets/Scripts/GameLoop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Debug.Log("This is Awake function");
+    }
+    void Start()
+    {
+        Debug.Log("This is Start function");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("This is Update function");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("This is LateUpdate function");
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log("This is FixedUpdate function");
+    }
+}
